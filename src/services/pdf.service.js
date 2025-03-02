b7481c7e102f431943fb3ff6d632b44b274efce0
lucho885 <lucholoidi2@gmail.com>
2025-03-02 18:26:44 +0100

    probando si funciona cambie la congif de puppeter en render y en el repo

--- src/services/pdf.service.js
+++ src/services/pdf.service.js
@@ -5,30 +5,27 @@ class PdfService {
   async generatePdfFromData(userData) {
     try {
       const browser = await puppeteer.launch({
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome-stable", // Ruta del navegador en Render
         headless: "new", // Para evitar problemas de compatibilidad
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
       });
-    
-      
+
       const page = await browser.newPage();
 
-      const contentHtml = generateStyle1Html(userData) 
-     
-      await page.setContent(contentHtml);
+      const contentHtml = generateStyle1Html(userData); // Generar HTML
 
-      // const pdfBuffer = await page.pdf({ format: "A4" });
-      const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
+      await page.setContent(contentHtml); // Establecer el contenido
 
+      const pdfBuffer = await page.pdf({ format: "A4", printBackground: true }); // Crear el PDF
 
       await browser.close();
 
-      return Buffer.from(pdfBuffer);
+      return Buffer.from(pdfBuffer); // Retornar el PDF en formato buffer
     } catch (error) {
       console.error("Error generando PDF:", error);
-      throw error; // Propaga el error al controlador
+      throw error; // Propagar el error al controlador
     }
   }
-
 }
 
 module.exports = new PdfService();
