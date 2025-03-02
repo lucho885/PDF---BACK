e8c6ab0b1fcf8f7eb2e6901c9508684fc695c246
lucho885 <lucholoidi2@gmail.com>
2025-03-02 17:30:48 +0100

    ruta get para ver que funcione el back

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 
+  
 
 const app = express();
 app.use(cors());
 // Aumentar el límite del JSON para aceptar imágenes en Base64 grandes
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-
+app.get("/", (req, res) => {
+    res.send("El backend está funcionando 🚀");
+  });
 app.use(express.json()); // Middleware para JSON
 app.use("/pdf", pdfRoutes); // Usa las rutas de usuarios
 
